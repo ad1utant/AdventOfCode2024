13a08bb732cba8452503ffa55d7541d9c50d26a2
ad1utant <mikolaj@marasek.eu>
2024-12-08 22:29:31 +0100

    part 2 of 5.12.2024 works for test data - no idea why does not work for input

--- 5/part_2.py
+++ 5/part_2.py
@@ -1,54 +1,34 @@
-pairs, lines, correct_lines = [], [], []
+pairs, lines, = [], []
 result, count = 0, 0
-with open('5_test_data.txt', 'r') as file:
+with open('5_data.txt', 'r') as file:
     for line in file:
         if '|' in line:
             pairs.append(line.strip('\n').split('|'))
         else:
             lines.append(line.strip('\n').split(','))
+
 for line in lines:
+    new_pairs = []
     for pair in pairs:
         previous = pair[0]
         next = pair[1]
         if previous in line and next in line:
-            previous_index = line.index(previous)
-            next_index = line.index(next)
-            if previous_index < next_index:
-                count += 1
-        else:
-            count += 1
-        if count == len(pairs):
-            lines.remove(line)
-    count = 0
-
-def transformation(line, pairs):
-    code = []
-    new_pairs = []
-    for pair in pairs:
-        if pair[0] in code and pair[1] in line:
-            new_pairs.append(pair)
-            if pair[0] not in code:
-                code.append(pair[0])
-            if pair[1] not in code:
-                code.append(pair[1])
-    print(f'line{line}')
-    print(f'code{code}')
-    count = 0
-    while count != len(pairs):
+            new_pairs.append([previous, next])
+    round_first = True
+    while count != len(new_pairs):
+        count = 0
         for pair in new_pairs:
             previous = pair[0]
             next = pair[1]
             previous_index = line.index(previous)
             next_index = line.index(next)
             if previous_index > next_index:
-                code.remove(next)
-                code.insert(previous_index, next)
-                continue
-            count += 1
-        if count == len(pairs):
-            break
-        count = 0
-    return code
+                round_first = False
+                line.remove(previous)
+                line.insert(next_index, previous)
+            else:
+                count += 1
+    if not round_first:
+        result += int(line[len(line) // 2])
 
-for line in lines:
-    print(transformation(line,pairs))
+print(result)
